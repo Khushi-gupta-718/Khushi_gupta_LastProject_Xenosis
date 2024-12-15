1733c0228cdaecec3b2bb9d56cf297fc3d21047e
Khushi-gupta-718 <118425831+Khushi-gupta-718@users.noreply.github.com>
2024-12-15 17:30:22 +0530

    Add files via upload

---
+++ iut-laval-order-management-main/src/main/java/fr/iut/Product.java
@@ -0,0 +1,36 @@
+package fr.iut;
+
+public class Product {
+    private String name;
+    private double price;
+    private int stockQuantity;
+
+    public Product(String name, double price, int stockQuantity) {
+        this.name = name;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        price = price * 1.01;
+        return price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void decrementStock() {
+        if (stockQuantity > 0) {
+            stockQuantity--;
+        }
+    }
+
+    public void incrementStock() {
+        stockQuantity++;
+    }
+}
---
+++ iut-laval-order-management-main/src/test/java/OrderManagementTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.Test;
+
+// import fr.iut.exceptions.InvalidDiscountCodeException;
+// import fr.iut.exceptions.OutOfStockException;
+// import fr.iut.Invoice;
+// import fr.iut.Order;
+// import fr.iut.Product;
+// import fr.iut.ShoppingCart;
+
+// import static org.junit.jupiter.api.Assertions.*;
+
+public class OrderManagementTest {
+    // TODO: Declare products and cart
+
+    @BeforeEach
+    void setUp() {
+        // TODO: Initialize products and cart
+    }
+
+    // TODO: Implement tests for Product
+
+    // TODO: Implement tests for ShoppingCart
+
+    // TODO: Implement tests for Invoice
+
+    // TODO: Implement tests for Order
+}
